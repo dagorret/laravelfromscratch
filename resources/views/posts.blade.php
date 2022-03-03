6c97934a66a1acbbdcc0718d9deb9a0f9ccf4806
Carlos Dagorret <dagorret@eco.unrc.edu.ar>
2022-03-03 20:53:14 -0300

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/css/app.css">
+
+<body>
+
+<article>
+    <h1>My First Post</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores assumenda, autem corporis debitis delectus deleniti dignissimos dolorem doloremque dolores dolorum ducimus ex fugit harum inventore ipsam itaque labore magni maiores maxime minima modi molestias nam natus nesciunt nobis nostrum officiis omnis perspiciatis provident, quaerat quos ratione similique ullam ut voluptas voluptatibus? Accusantium assumenda beatae consequatur, distinctio doloribus ea ex exercitationem explicabo facere fugiat laborum minima nesciunt nihil, odio porro praesentium quam quia quidem quisquam quo repellendus suscipit tempore ut voluptatem voluptatum! Ducimus eaque facere nam repellendus reprehenderit? Animi corporis dolorem facilis id itaque laborum nihil obcaecati quibusdam quis voluptates?
+    </p>
+</article>
+
+<article>
+    <h1>My Second Post</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores assumenda, autem corporis debitis delectus deleniti dignissimos dolorem doloremque dolores dolorum ducimus ex fugit harum inventore ipsam itaque labore magni maiores maxime minima modi molestias nam natus nesciunt nobis nostrum officiis omnis perspiciatis provident, quaerat quos ratione similique ullam ut voluptas voluptatibus? Accusantium assumenda beatae consequatur, distinctio doloribus ea ex exercitationem explicabo facere fugiat laborum minima nesciunt nihil, odio porro praesentium quam quia quidem quisquam quo repellendus suscipit tempore ut voluptatem voluptatum! Ducimus eaque facere nam repellendus reprehenderit? Animi corporis dolorem facilis id itaque laborum nihil obcaecati quibusdam quis voluptates?
+    </p>
+
+
+</body>
